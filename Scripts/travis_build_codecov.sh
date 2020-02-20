f5d242b22bcba7aee37e0cc2c40d6e01f9e9e791
Justin <jwkim98@kaist.ac.kr>
2020-02-21 00:00:16 +0900

    refactor : set project name to TakiCar

--- Scripts/travis_build_codecov.sh
+++ Scripts/travis_build_codecov.sh
@@ -16,14 +16,14 @@ mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_COVERAGE=ON
 make UnitTests
-lcov --gcov-tool /usr/bin/gcov-8 -c -i -d Tests/UnitTests -o base.info
+lcov --gcov-tool /usr/bin/gcov-7 -c -i -d Tests/UnitTests -o base.info
 bin/UnitTests
-lcov --gcov-tool /usr/bin/gcov-8 -c -d Tests/UnitTests -o test.info
-lcov --gcov-tool /usr/bin/gcov-8 -a base.info -a test.info -o coverage.info
-lcov --gcov-tool /usr/bin/gcov-8 -r coverage.info '/usr/*' -o coverage.info
-lcov --gcov-tool /usr/bin/gcov-8 -r coverage.info '*/Includes/*' -o coverage.info
-lcov --gcov-tool /usr/bin/gcov-8 -r coverage.info '*/Libraries/*' -o coverage.info
-lcov --gcov-tool /usr/bin/gcov-8 -l coverage.info
+lcov --gcov-tool /usr/bin/gcov-7 -c -d Tests/UnitTests -o test.info
+lcov --gcov-tool /usr/bin/gcov-7 -a base.info -a test.info -o coverage.info
+lcov --gcov-tool /usr/bin/gcov-7 -r coverage.info '/usr/*' -o coverage.info
+lcov --gcov-tool /usr/bin/gcov-7 -r coverage.info '*/Includes/*' -o coverage.info
+lcov --gcov-tool /usr/bin/gcov-7 -r coverage.info '*/Libraries/*' -o coverage.info
+lcov --gcov-tool /usr/bin/gcov-7 -l coverage.info
 
 curl -s https://codecov.io/bash > .codecov
 chmod +x .codecov
